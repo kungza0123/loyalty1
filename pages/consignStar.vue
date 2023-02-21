67df2364803b981571b8b572ce29f2ecc6993a22
kungza0123 <6331305002@lamduan.mfu.ac.th>
2023-02-22 02:04:38 +0700

    jj

--- pages/consignStar.vue
+++ pages/consignStar.vue
@@ -6,16 +6,25 @@
       :style="{ top: $vuetify.application.top + 'px', zIndex: 6 }">
       <v-list dense>
         <v-spacer></v-spacer>
-        <v-list-item @click="d">
-          News feed
+        <v-list-item>
+          <v-card-actions>
+            <v-btn to="/home">
+
+              News feed
+            </v-btn>
+          </v-card-actions>
         </v-list-item>
         <v-spacer></v-spacer>
-        <v-list-item @click="f">
-          Consign, Reward, Stars point
+        <v-list-item>
+          <v-card-actions>
+            <v-btn to="/consignStar">
+              Consign, Reward, Stars point
+            </v-btn>
+          </v-card-actions>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar app color="#B22222" dark src="https://picsum.photos/1920/1080?random" fade-img-on-scroll shrink-on-scroll>
+    <v-app-bar app color="#B22222" dark :src="require('../img/logo1.jpg')" fade-img-on-scroll shrink-on-scroll>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"> </v-app-bar-nav-icon>
       <v-app-bar-title>Loyalty Program</v-app-bar-title>
       <v-spacer></v-spacer>
@@ -24,9 +33,13 @@
       <v-row>
         <v-col>
           <div class="d-flex align-center">
-            <v-avatar color="#000000" size="40">
-              <v-icon dark>mdi-arrow-left-circle</v-icon>
-            </v-avatar>
+            <v-card-actions>
+              <v-avatar color="#000000" size="40" to="/home">
+                <v-btn to="/home">
+                  <v-icon dark>mdi-arrow-left-circle</v-icon>
+                </v-btn>
+              </v-avatar>
+            </v-card-actions>
           </div>
         </v-col>
       </v-row>
@@ -39,7 +52,8 @@
           <div v-resize-text>
             <h5>If you want to consign stars.</h5>
           </div>
-        </v-col><v-col></v-col>
+        </v-col>
+        <v-col></v-col>
       </v-row>
       <v-row>
         <v-col></v-col>
@@ -47,10 +61,11 @@
           <v-btn>
             send stars
           </v-btn>
-        </v-col><v-col></v-col>
+        </v-col>
+        <v-col></v-col>
       </v-row>
       <v-row>
-        <v-col >
+        <v-col>
 
         </v-col>
       </v-row>
@@ -59,7 +74,8 @@
           <div v-resize-text>
             <h5>Reward Exchange :</h5>
           </div>
-        </v-col><v-col></v-col>
+        </v-col>
+        <v-col></v-col>
       </v-row>
 
     </v-container>
@@ -67,110 +83,110 @@
       <v-container>
         <v-row>
 
-            <v-col cols="12" md="6" lg="4">
-                <v-hover v-slot:default="{ hover }" open-delay="50" close-delay="50">
-                  <div>
-                    <v-card flat :color="hover ? 'white' : 'transparent'" :elevation="hover ? 12 : 0" hover to="/history">
-                      <v-img
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa4xkBzGDQyj4D0ymgAdBqe9cFsbFYorqfSg&usqp=CAU"
-                        :aspect-ratio="16 / 9" gradient="to top, rgba(25,32,72,.4), rgba(25,32,72,.0)" height="200px"
-                        class="elevation-2" style="border-radius: 16px">
-                        <!-- <v-card-text>
+          <v-col cols="12" md="6" lg="4">
+            <v-hover v-slot:default="{ hover }" open-delay="50" close-delay="50">
+              <div>
+                <v-card flat :color="hover ? 'white' : 'transparent'" :elevation="hover ? 12 : 0" hover to="/history">
+                  <v-img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa4xkBzGDQyj4D0ymgAdBqe9cFsbFYorqfSg&usqp=CAU"
+                    :aspect-ratio="16 / 9" gradient="to top, rgba(25,32,72,.4), rgba(25,32,72,.0)" height="200px"
+                    class="elevation-2" style="border-radius: 16px">
+                    <!-- <v-card-text>
                           <v-btn color="accent">Sold out</v-btn>
                         </v-card-text> -->
-                      </v-img>
+                  </v-img>
 
-                      <v-card-text>
-                        <div class="text-h5 font-weight-bold primary--text">
-                          Train Tracks
-                        </div>
+                  <v-card-text>
+                    <div class="text-h5 font-weight-bold primary--text">
+                      Train Tracks
+                    </div>
 
-                        <div class="text-body-1 py-4">Description
-                          'Nature with Train Tracks'
+                    <div class="text-body-1 py-4">Description
+                      'Nature with Train Tracks'
 
-                          </div>
-                        <div class="d-flex align-center">
-                          <v-avatar color="accent" size="36">
-                            <v-icon dark>mdi-star</v-icon>
-                          </v-avatar>
+                    </div>
+                    <div class="d-flex align-center">
+                      <v-avatar color="accent" size="36">
+                        <v-icon dark>mdi-star</v-icon>
+                      </v-avatar>
 
-                          <div class="pl-2">12.00</div>
-                        </div>
-                      </v-card-text>
-                    </v-card>
-                  </div>
-                </v-hover>
-              </v-col>
+                      <div class="pl-2">12.00</div>
+                    </div>
+                  </v-card-text>
+                </v-card>
+              </div>
+            </v-hover>
+          </v-col>
 
-              <v-col cols="12" md="6" lg="4">
-                <v-hover v-slot:default="{ hover }" open-delay="50" close-delay="50">
-                  <div>
-                    <v-card flat :color="hover ? 'white' : 'transparent'" :elevation="hover ? 12 : 0" hover to="/history">
-                      <v-img
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1MY-Jyz53_qGn1eC-2FMx85R5F61Dm_TBZw&usqp=CAU"
-                        :aspect-ratio="16 / 9" gradient="to top, rgba(25,32,72,.4), rgba(25,32,72,.0)" height="200px"
-                        class="elevation-2" style="border-radius: 16px">
-                        <!-- <v-card-text>
+          <v-col cols="12" md="6" lg="4">
+            <v-hover v-slot:default="{ hover }" open-delay="50" close-delay="50">
+              <div>
+                <v-card flat :color="hover ? 'white' : 'transparent'" :elevation="hover ? 12 : 0" hover to="/history">
+                  <v-img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1MY-Jyz53_qGn1eC-2FMx85R5F61Dm_TBZw&usqp=CAU"
+                    :aspect-ratio="16 / 9" gradient="to top, rgba(25,32,72,.4), rgba(25,32,72,.0)" height="200px"
+                    class="elevation-2" style="border-radius: 16px">
+                    <!-- <v-card-text>
                           <v-btn color="accent">Sold out</v-btn>
                         </v-card-text> -->
-                      </v-img>
+                  </v-img>
 
-                      <v-card-text>
-                        <div class="text-h5 font-weight-bold primary--text">
-                          Leaf
-                        </div>
+                  <v-card-text>
+                    <div class="text-h5 font-weight-bold primary--text">
+                      Leaf
+                    </div>
 
-                        <div class="text-body-1 py-4">Description
-                          'Drop of wateron leaf'
+                    <div class="text-body-1 py-4">Description
+                      'Drop of wateron leaf'
 
-                          </div>
-                        <div class="d-flex align-center">
-                          <v-avatar color="accent" size="36">
-                            <v-icon dark>mdi-star</v-icon>
-                          </v-avatar>
+                    </div>
+                    <div class="d-flex align-center">
+                      <v-avatar color="accent" size="36">
+                        <v-icon dark>mdi-star</v-icon>
+                      </v-avatar>
 
-                          <div class="pl-2">24.12</div>
-                        </div>
-                      </v-card-text>
-                    </v-card>
-                  </div>
-                </v-hover>
-              </v-col>
+                      <div class="pl-2">24.12</div>
+                    </div>
+                  </v-card-text>
+                </v-card>
+              </div>
+            </v-hover>
+          </v-col>
 
-              <v-col cols="12" md="6" lg="4">
-                <v-hover v-slot:default="{ hover }" open-delay="50" close-delay="50">
-                  <div>
-                    <v-card flat :color="hover ? 'white' : 'transparent'" :elevation="hover ? 12 : 0" hover to="/history">
-                      <v-img
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfvhC52821-wSuPOn7jA2pZXSzzY15HnSPiA&usqp=CAU"
-                        :aspect-ratio="16 / 9" gradient="to top, rgba(25,32,72,.4), rgba(25,32,72,.0)" height="200px"
-                        class="elevation-2" style="border-radius: 16px">
-                        <!-- <v-card-text>
+          <v-col cols="12" md="6" lg="4">
+            <v-hover v-slot:default="{ hover }" open-delay="50" close-delay="50">
+              <div>
+                <v-card flat :color="hover ? 'white' : 'transparent'" :elevation="hover ? 12 : 0" hover to="/history">
+                  <v-img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfvhC52821-wSuPOn7jA2pZXSzzY15HnSPiA&usqp=CAU"
+                    :aspect-ratio="16 / 9" gradient="to top, rgba(25,32,72,.4), rgba(25,32,72,.0)" height="200px"
+                    class="elevation-2" style="border-radius: 16px">
+                    <!-- <v-card-text>
                           <v-btn color="accent">Sold out</v-btn>
                         </v-card-text> -->
-                      </v-img>
+                  </v-img>
 
-                      <v-card-text>
-                        <div class="text-h5 font-weight-bold primary--text">
-                          Lake
-                        </div>
+                  <v-card-text>
+                    <div class="text-h5 font-weight-bold primary--text">
+                      Lake
+                    </div>
 
-                        <div class="text-body-1 py-4">Description
-                          'Mountain of the lake'
+                    <div class="text-body-1 py-4">Description
+                      'Mountain of the lake'
 
-                          </div>
-                        <div class="d-flex align-center">
-                          <v-avatar color="accent" size="36">
-                            <v-icon dark>mdi-star</v-icon>
-                          </v-avatar>
+                    </div>
+                    <div class="d-flex align-center">
+                      <v-avatar color="accent" size="36">
+                        <v-icon dark>mdi-star</v-icon>
+                      </v-avatar>
 
-                          <div class="pl-2">48.24</div>
-                        </div>
-                      </v-card-text>
-                    </v-card>
-                  </div>
-                </v-hover>
-              </v-col>
+                      <div class="pl-2">48.24</div>
+                    </div>
+                  </v-card-text>
+                </v-card>
+              </div>
+            </v-hover>
+          </v-col>
 
         </v-row>
       </v-container>
@@ -193,8 +209,7 @@ export default {
 };
 </script>
 
-<style>
-main {
+<style>main {
   background-image: url('../img/back.png');
   background-attachment: fixed;
   background-position: center;
--- pages/home.vue
+++ pages/home.vue
@@ -1,102 +1,109 @@
 <template>
   <v-app id="inspire">
-        <v-overlay :value="drawer" z-index="4">
+    <v-overlay :value="drawer" z-index="4">
     </v-overlay>
-    <v-navigation-drawer v-model="drawer" app clipped hide-overlay :style="{ top: $vuetify.application.top + 'px', zIndex: 6 }">
+    <v-navigation-drawer v-model="drawer" app clipped hide-overlay
+      :style="{ top: $vuetify.application.top + 'px', zIndex: 6 }">
       <v-list dense>
         <v-spacer></v-spacer>
-        <v-list-item @click="d">
-            News feed
+        <v-list-item>
+          <v-card-actions>
+            <v-btn to="/home">
+
+              News feed
+            </v-btn>
+          </v-card-actions>
         </v-list-item>
         <v-spacer></v-spacer>
-        <v-list-item @click="f">
-            Consign, Reward, Stars point
+        <v-list-item>
+          <v-card-actions>
+            <v-btn to="/consignStar">
+              Consign, Reward, Stars point
+            </v-btn>
+          </v-card-actions>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      app
-      color="#B22222"
-      dark
-      :src="require('../img/logo1.jpg')"
-      
-      fade-img-on-scroll
-      shrink-on-scroll
-    >
-      <v-app-bar-nav-icon  @click.stop="drawer = !drawer"> </v-app-bar-nav-icon>
+    <v-app-bar app color="#B22222" dark :src="require('../img/logo1.jpg')" fade-img-on-scroll shrink-on-scroll>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"> </v-app-bar-nav-icon>
       <v-app-bar-title>Loyalty Program</v-app-bar-title>
       <v-spacer></v-spacer>
-      
-      
+
     </v-app-bar>
     <v-sheet>
-      <v-container >
+      <v-container>
         <v-container fluid>
-      <v-layout row wrap>
-        <v-flex xs12 sm6 md4 lg3 v-for="item in newsItems" :key="item.id">
-          <v-card>
-            <v-img :src="item.image" aspect-ratio="1"></v-img>
-            <v-card-title>{{ item.title }}</v-card-title>
-            <v-card-text>{{ item.content }}</v-card-text>
-            <v-card-actions>
-              <v-btn text color="primary">Read more</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
+          <v-layout row wrap>
+            <v-flex xs12 sm6 md4 lg3 v-for="item in newsItems" :key="item.id">
+              <v-card>
+                <v-img :src="item.image" aspect-ratio="1"></v-img>
+                <v-card-title>{{ item.title }}</v-card-title>
+                <v-card-text>{{ item.content }}</v-card-text>
+                <v-card-actions>
+                  <v-btn text color="primary">Read more</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
       </v-container>
     </v-sheet>
-    
+
   </v-app>
 </template>
 
-  
-  <script>
-  export default {
-    data() {
-      return {
-        drawer: true,
-        newsItems: [
-          {
-            id: 1,
-            title: 'Lorem ipsum dolor sit amet',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.',
-            image: 'https://via.placeholder.com/400x400',
-          },
-          {
-            id: 2,
-            title: 'Pellentesque nec ipsum nec dolor',
-            content: 'Pellentesque nec ipsum nec dolor dignissim convallis. Donec sit amet ligula enim. Duis vel nibh at velit scelerisque suscipit. Sed augue ipsum, egestas nec, vestibulum et, malesuada adipiscing, dui. In vel dui.',
-            image: 'https://via.placeholder.com/400x400',
-          },
-          {
-            id: 3,
-            title: 'Vestibulum ante ipsum primis in faucibus orci',
-            content: 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.',
-            image: 'https://via.placeholder.com/400x400',
-          },
-          {
-            id: 4,
-            title: 'Morbi in sem quis dui placerat ornare',
-            content: 'Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat.',
-            image: 'https://via.placeholder.com/400x400',
-          }
-        ]
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      newsItems: [{
+        id: 1,
+        title: 'Lorem ipsum dolor sit amet',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.',
+        image: 'https://via.placeholder.com/400x400',
+      },
+      {
+        id: 2,
+        title: 'Pellentesque nec ipsum nec dolor',
+        content: 'Pellentesque nec ipsum nec dolor dignissim convallis. Donec sit amet ligula enim. Duis vel nibh at velit scelerisque suscipit. Sed augue ipsum, egestas nec, vestibulum et, malesuada adipiscing, dui. In vel dui.',
+        image: 'https://via.placeholder.com/400x400',
+      },
+      {
+        id: 3,
+        title: 'Vestibulum ante ipsum primis in faucibus orci',
+        content: 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.',
+        image: 'https://via.placeholder.com/400x400',
+      },
+      {
+        id: 4,
+        title: 'Morbi in sem quis dui placerat ornare',
+        content: 'Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat.',
+        image: 'https://via.placeholder.com/400x400',
       }
+      ],
+      methods: {
+        newfeed() {
+          this.$router.push("/home");
+        },
+        con() {
+          this.$router.push("/consignStar");
+        },
+      },
     }
   }
-  </script>
-  <style>
-  main{
-    /* background-color: #000000;
+}
+</script>
+
+<style>
+main {
+  /* background-color: #000000;
     background-image: url('./assets/banner.jpg'); */
-   
-   background-image:url('../img/back.png');
-   background-attachment: fixed;
+
+  background-image: url('../img/back.png');
+  background-attachment: fixed;
   background-position: center;
   background-repeat: no-repeat;
- /* background-size: 100%;  */
- }
-  </style>
-  
+  /* background-size: 100%;  */
+}
+</style>
